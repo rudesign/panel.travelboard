c76ebe3431e0611eef4b991c67066c0ce8570e43
Alex <m1@cometstudio.ru>
2014-12-24 02:12:02 +0300

    user controller added

--- app/config/loader.php
+++ app/config/loader.php
@@ -9,6 +9,7 @@ $loader->registerDirs(
     array(
         $config->application->controllersDir,
         $config->application->modelsDir,
-        $config->application->helpersDir
+        $config->application->helpersDir,
+        $config->application->libraryDir
     )
 )->register();
