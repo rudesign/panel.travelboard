da46ddb778a914fdc2f1fc3be4012b4f36526263
Alex <m1@cometstudio.ru>
2014-12-28 19:54:47 +0300

    stat grid added

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -4,6 +4,8 @@ class IndexController extends ViewsController
 {
     public function indexAction()
     {
-
+        $this->view->setVar('totalCountries', 9001);
+        $this->view->setVar('totalCities', 9002);
+        $this->view->setVar('totalHotels', 9003);
     }
 }
